7860421d6906b7f298a1713bc774e0ba5b9c4917
fault <critical@fault.io>
2021-06-08 00:14:30 -0700

    Use libexec binaries identified by root.query.

--- operations.py
+++ operations.py
@@ -13,6 +13,8 @@ from fault.transcript import execution
 from fault.system.execution import KInvocation
 from fault.project import graph
 
+from ..root import query
+
 def update(pd:root.Product):
 	pd.clear()
 	pd.update()
@@ -25,12 +27,14 @@ def plan_build(command, ccontext:files.Path, intention:str, cache:files.Path, ar
 
 	pj = pcontext.project(identifier)
 	project = pj.factor
-	exepath = str(ccontext / 'execute')
+	env, exepath, xargs = query.dispatch('factors-cc')
 
 	dims = (str(project),)
 	xid = '/'.join(dims)
 
-	ki = KInvocation(exepath, [exepath, str(ccontext), str(cache), str(pj.product.route), str(project)] + argv)
+	ki = KInvocation(xargs[0], xargs + [
+		str(ccontext), str(cache), str(pj.product.route), str(project)
+	] + argv)
 	yield ('FPI', dims, xid, None, ki)
 
 def plan_test(intention:str, argv, pcontext:root.Context, identifier):
@@ -40,21 +44,20 @@ def plan_test(intention:str, argv, pcontext:root.Context, identifier):
 
 	pj = pcontext.project(identifier)
 	project = pj.factor
-	exepath = '/usr/local/bin/pyx'
+
+	from ..root import query
+	exeenv, exepath, xargs = query.dispatch('python')
 
 	for (fp, ft), fd in pj.select(root.types.factor@'test'):
 		if not fp.identifier.startswith('test_'):
 			continue
 
-		cmd = [
-			exepath,
-			'fault.test.bin.coherence',
-			str(project), str(fp)
-		]
+		cmd = xargs + ['fault.test.bin.coherence', str(project), str(fp)]
 		env = dict(os.environ)
-		env['FACTORPATH'] = '/x/redirect-fault:' + os.environ['PRODUCT']
+		env.update(exeenv)
+		env['PRODUCT'] = str(pj.product.route)
 		env['PROJECT'] = str(project)
-		ki = KInvocation(exepath, cmd, environ=env)
+		ki = KInvocation(str(exepath), cmd, environ=env)
 		dims = (str(project), str(fp))
 		xid = '/'.join(dims)
 		yield ('Fates', dims, xid, None, ki)
@@ -75,7 +78,9 @@ def iterconstructs(pd:root.Product, contexts:typing.Iterable[files.Path], intent
 			q.finish(pj.identifier)
 
 # Build the product or a project set.
-def build(traps, ctx, status, pd:root.Product, contexts:typing.Iterable[files.Path], intention:str, cache:files.Path, argv=None, rebuild=False):
+def build(traps, ctx, status, pd:root.Product,
+	contexts:typing.Iterable[files.Path], intention:str,
+	cache:files.Path, argv=None, rebuild=False):
 	"""
 	# Build all projects within product using the &contexts.
 	"""
